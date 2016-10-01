9fd16b21ee058a04fa337ce866b426b4773829c0
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2016-10-01 15:02:06 -0400

    added Jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-})
 
+if(process.env.JAWSDB_URL){
+	var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+	var connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	})
+}
 connection.connect(function(err){
 	if(err) {
 		console.log("Error connection: "+err.stack)
